5ad04bc9c1849e22e3df8f96a418af80110a8b50
Nhung Le <nhl256@nyu.edu>
2020-05-07 16:13:30 -0400

    update submission files

--- ObjectDetection/fastRCNN/run_test.py
+++ ObjectDetection/fastRCNN/run_test.py
@@ -12,10 +12,15 @@ import torchvision
 from data_helper import LabeledDataset
 from helper import compute_ats_bounding_boxes, compute_ts_road_map
 
-from model_loader import get_transform, ModelLoader
+from model_loader import get_transform_task1, get_transform_task2, ModelLoader
 
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
+
+random.seed(0)
+np.random.seed(0)
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', type=str, default='data')
@@ -31,15 +36,30 @@ if opt.testset:
 else:
     labeled_scene_index = np.arange(120, 134)
 
-labeled_trainset = LabeledDataset(
+# For bounding boxes task
+labeled_trainset_task1 = LabeledDataset(
     image_folder=image_folder,
     annotation_file=annotation_csv,
     scene_index=labeled_scene_index,
-    transform=get_transform(),
+    transform=get_transform_task1(),
     extra_info=False
     )
-dataloader = torch.utils.data.DataLoader(
-    labeled_trainset,
+dataloader_task1 = torch.utils.data.DataLoader(
+    labeled_trainset_task1,
+    batch_size=1,
+    shuffle=False,
+    num_workers=4
+    )
+# For road map task
+labeled_trainset_task2 = LabeledDataset(
+    image_folder=image_folder,
+    annotation_file=annotation_csv,
+    scene_index=labeled_scene_index,
+    transform=get_transform_task2(),
+    extra_info=False
+    )
+dataloader_task2 = torch.utils.data.DataLoader(
+    labeled_trainset_task2,
     batch_size=1,
     shuffle=False,
     num_workers=4
@@ -50,22 +70,29 @@ model_loader = ModelLoader()
 total = 0
 total_ats_bounding_boxes = 0
 total_ts_road_map = 0
-for i, data in enumerate(dataloader):
-    total += 1
-    sample, target, road_image = data
-    sample = sample.cuda()
+with torch.no_grad():
+    for i, data in enumerate(dataloader_task1):
+        total += 1
+        sample, target, road_image = data
+        sample = sample.cuda()
 
-    predicted_bounding_boxes = model_loader.get_bounding_boxes(sample)[0].cpu()
-    predicted_road_map = model_loader.get_binary_road_map(sample).cpu()
+        predicted_bounding_boxes = model_loader.get_bounding_boxes(sample)[0].cpu()
+        ats_bounding_boxes = compute_ats_bounding_boxes(predicted_bounding_boxes, target['bounding_box'][0])
+        total_ats_bounding_boxes += ats_bounding_boxes
 
-    ats_bounding_boxes = compute_ats_bounding_boxes(predicted_bounding_boxes, target['bounding_box'][0])
-    ts_road_map = compute_ts_road_map(predicted_road_map, road_image)
+        if opt.verbose:
+            print(f'{i} - Bounding Box Score: {ats_bounding_boxes:.4}')
 
-    total_ats_bounding_boxes += ats_bounding_boxes
-    total_ts_road_map += ts_road_map
+    for i, data in enumerate(dataloader_task2):
+        sample, target, road_image = data
+        sample = sample.cuda()
 
-    if opt.verbose:
-        print(f'{i} - Bounding Box Score: {ats_bounding_boxes:.4} - Road Map Score: {ts_road_map:.4}')
+        predicted_road_map = model_loader.get_binary_road_map(sample).cpu()
+        ts_road_map = compute_ts_road_map(predicted_road_map, road_image)
+        total_ts_road_map += ts_road_map
+
+        if opt.verbose:
+            print(f'{i} - Road Map Score: {ts_road_map:.4}')
 
 print(f'{model_loader.team_name} - {model_loader.round_number} - Bounding Box Score: {total_ats_bounding_boxes / total:.4} - Road Map Score: {total_ts_road_map / total:.4}')
     
