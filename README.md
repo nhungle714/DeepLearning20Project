e4affa9062b45c56f6ec1add64c2f8af58e6e499
Hengyu Tang <tanghengyu@gmail.com>
2020-05-12 16:46:56 -0400

    add_final_train

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DeepLearning20Project
+This repository contains the implementation of DS-GA 1008 Deep Learning final project for Team 32: AwesomeThree.
+
+### Navigation
+To reproduce results of our submitted model:
+Object detection train file: `/ObjectDetection/preTask_fastRCNN/objectDetectionFastRCNN_050820.ipynb`
+Roadmap train file: `/Roadmap/roadmap_best_model.ipynb`
+Relevant self-sueprvised methods can be found in folder: `/Self-Supervised/`, while model fine-tuning using pretask weights are under `/ObjectDetection/` and `/Roadmap/`.
---
+++ Roadmap/VAE_lane.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn as nn
+import numpy as np 
+
+class encoder_for_vae(nn.Module):
+
+    def __init__(self):
+        super(encoder_for_vae, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(2048, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)
+        )
+
+        self.mu_dec = nn.Linear(18432, 512)
+        self.logvar_dec = nn.Linear(18432, 512)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(-1, 18432)
+        mu = self.mu_dec(x)
+        logvar = self.logvar_dec(x)
+
+        return mu, logvar
+    
+def get_upsampling_weight(in_channels, out_channels, kernel_size):
+    """Make a 2D bilinear kernel suitable for upsampling"""
+    factor = (kernel_size + 1) // 2
+    if kernel_size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = np.ogrid[:kernel_size, :kernel_size]
+    filt = (1 - abs(og[0] - center) / factor) * \
+           (1 - abs(og[1] - center) / factor)
+    weight = np.zeros((in_channels, out_channels, kernel_size, kernel_size),
+                      dtype=np.float64)
+    weight[range(in_channels), range(out_channels), :, :] = filt
+    return torch.from_numpy(weight).float()
+
+class _DecoderBlock(nn.Module):
+    """
+    Taken from https://github.com/zijundeng/pytorch-semantic-segmentation/blob/master/models/seg_net.py
+    """
+    def __init__(self, in_channels, out_channels, num_conv_layers, out_activation = 'relu'):
+        super(_DecoderBlock, self).__init__()
+        middle_channels = int(in_channels / 2)
+        layers = [
+            nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2),
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(middle_channels),
+            nn.ReLU(inplace=True)
+        ]
+        layers += [
+                      nn.Conv2d(middle_channels, middle_channels, kernel_size=3, padding=1),
+                      nn.BatchNorm2d(middle_channels),
+                      nn.ReLU(inplace=True),
+                  ] * (num_conv_layers - 2)
+        layers += [
+            nn.Conv2d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True) if out_activation == 'relu' else nn.Sigmoid(),
+        ]
+        self.decode = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.decode(x)
+class decoder_conv(nn.Module):
+    def __init__(self):
+        super(decoder_conv, self).__init__()
+        self.dec1 = _DecoderBlock(128, 256,2)
+        self.dec2 = _DecoderBlock(256, 256,2)
+        self.dec3 = _DecoderBlock(256, 256, 2)
+        self.dec4 = _DecoderBlock(256, 256, 2)
+        self.dec5 = _DecoderBlock(256 ,256, 2)
+        self.dec6 = _DecoderBlock(256, 128, 2)
+        self.conv_out = nn.Conv2d(128, 1,3 , padding=1)
+        self.final_upsample = nn.Upsample((800, 800), mode='bilinear', align_corners=False)
+        self.sigmoid = nn.Sigmoid()
+        self._initialize_weights()
+        
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.ConvTranspose2d):
+                assert m.kernel_size[0] == m.kernel_size[1]
+                initial_weight = get_upsampling_weight(
+                    m.in_channels, m.out_channels, m.kernel_size[0])
+                m.weight.data.copy_(initial_weight)
+                
+    def forward(self, x):
+        x = x.view(-1, 128, 2, 2)
+        x = self.dec1(x)
+        x= self.dec2(x)
+        x = self.dec3(x)
+        x = self.dec4(x)
+        x = self.dec5(x)
+        x = self.dec6(x)
+        x = self.conv_out(x)
+        x = self.final_upsample(x)
+        x = self.sigmoid(x)
+        return x
+class vae_mapping(nn.Module):
+    def __init__(self):
+        super(vae_mapping, self).__init__()
+        
+        self.feature_extractor = torchvision.models.resnet50(pretrained = False)
+        self.encoder = [nn.Conv2d(1, 64, kernel_size=(7,7), stride=(2,2), padding=(3,3), 
+                                 bias = False)] + list(self.feature_extractor.children())[1:-2]
+        self.encoder = nn.Sequential(*self.encoder)
+        self.encoder_for_vae = encoder_for_vae()
+        self.decoder = decoder_conv()
+        
+    def reparameterize(self, is_training, mu, logvar):
+        if is_training:
+            std = torch.exp(0.5*logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+        
+    def forward(self, x, is_training, defined_mu = None):
+        feature_maps = self.encoder(x)
+        mu, logvar = self.encoder_for_vae(feature_maps)
+        z = self.reparameterize(is_training, mu, logvar)
+        
+        pred_map = self.decoder(z)
+        
+        return pred_map, mu, logvar
